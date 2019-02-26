72b4bb81974723443a57a4c7fc702f2272929e51
--set <elfoton@gmail.com>
2019-02-26 01:18:52 -0300

    Fix eq rule

--- src/Utils.js
+++ src/Utils.js
@@ -5,7 +5,7 @@ export default {
 		target.setDate(target.getDate() - dayNr + 3);
 		var firstThursday = target.valueOf();
 		target.setMonth(0, 1);
-		if(target.getDay() != 4) {
+		if(target.getDay() !== 4) {
 			target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
 		}
 		return 1 + Math.ceil((firstThursday - target) / 604800000);
